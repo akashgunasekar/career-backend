ed4fb73de4b43a6023f2d3fab0d79194d7833fdd
akashgunasekar <akashgunasekard7@gmail.com>
2026-01-11 20:41:50 +0530

    connect aiven mysql

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,29 @@
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
 
-dotenv.config();
+import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "career_app"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-export default db;
+
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// export const db = mysql.createPool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASSWORD || "",
+//   database: process.env.DB_NAME || "career_app"
+// });
+
+// export default db;
